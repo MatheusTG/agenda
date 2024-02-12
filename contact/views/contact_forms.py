0a92e0312be7913d8681cf2e1e32ca0196ad3264
Matheus <matheusteodorogarcia@gmail.com>
2024-02-12 15:31:04 -0300

    Formulário para inserção de contatos

---
+++ contact/views/contact_forms.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+from contact.forms import ContactForm
+
+def create(request):
+  if request.method == 'POST':
+    context = {
+      'form': ContactForm(request.POST)
+    }
+
+    return render(request, 'contact/create.html', context)
+
+  return render(request, 'contact/create.html', context)
--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.db.models import Q
-from contact.models import Contact
 from django.core.paginator import Paginator
+
+from contact.models import Contact
 
 def index(request):
   contacts = Contact.objects.filter(show=True).order_by('-id')
