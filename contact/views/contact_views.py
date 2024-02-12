e823865e7e955350ddd64ca3329673102ccfba4e
Matheus <matheusteodorogarcia@gmail.com>
2024-02-12 10:42:09 -0300

    Search de contatos

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.db.models import Q
 from contact.models import Contact
 from django.http import Http404
 
@@ -12,8 +13,28 @@ def index(request):
 
   return render(request, 'contact/index.html', context)
 
+def search(request):
+  search_value = request.GET.get('q', '').strip()
+  contacts = Contact.objects \
+    .filter(show=True) \
+    .filter(
+      Q(first_name__icontains=search_value) |
+      Q(last_name__icontains=search_value) |
+      Q(phone__icontains=search_value) |
+      Q(email__icontains=search_value)
+    ).order_by('-id')
+
+  if search_value == '':
+    return redirect('contact:index')
+
+  context = {
+    'contacts': contacts,
+    'site_title': 'Contatos - ',
+  }
+
+  return render(request, 'contact/index.html', context)
+
 def contact(request, contact_id):
-  # single_contact = Contact.objects.filter(pk=contact_id).first()
   single_contact = get_object_or_404(Contact, pk=contact_id, show=True)
 
   site_title = f'{single_contact.first_name} {single_contact.last_name} - '
